a6246b2578eec4315ee0032ad28ce959a7b938e9
Robert Dominguez <dominguez.1st@gmail.com>
2020-11-04 11:53:42 -0700

    Added support for View DDL.

--- src/edu/cnm/deepdive/model/Schema.java
+++ src/edu/cnm/deepdive/model/Schema.java
@@ -4,7 +4,9 @@ import com.google.gson.annotations.Expose;
 import java.util.stream.Stream;
 
 public class Schema {
-  private static final String PLACEHOLDER = "${TABLE_NAME}";
+
+  private static final String ENTITY_PLACEHOLDER = "${TABLE_NAME}";
+  private final static String VIEW_PLACEHOLDER = "${VIEW_NAME}";
 
   @Expose
   private Database database;
@@ -18,14 +20,19 @@ public class Schema {
   }
 
   public Stream<String> getDdl() {
-    return database.getEntities().stream()
-        .flatMap((entity) ->
-            Stream.concat(
-                Stream.of(entity.getDdl()),
-                entity.getIndices().stream()
-                    .map(Index::getDdl)
-            )
-            .map((s) -> s.replace(PLACEHOLDER, entity.getName()))
-        );
+    return Stream.concat(
+        database.getEntities().stream()
+            .flatMap((entity) ->
+                Stream.concat(
+                    Stream.of(entity.getDdl()),
+                    entity.getIndices().stream()
+                        .map(Index::getDdl)
+                )
+                    .map((s) -> s.replace(ENTITY_PLACEHOLDER, entity.getName()))
+            ),
+        database.getViews().stream()
+            .map((view) -> view.getDdl().replace(VIEW_PLACEHOLDER, view.getName()))
+    );
   }
+
 }
